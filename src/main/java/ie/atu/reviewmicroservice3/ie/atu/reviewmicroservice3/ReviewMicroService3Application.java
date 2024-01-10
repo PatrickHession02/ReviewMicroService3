49fa28301f948d145d4573a1bb18d0e1000f5f1c
PATRICK HESSION <G00394619@gmit.ie>
2024-01-10 15:48:32 +0000

    Initial commit

---
+++ src/main/java/ie/atu/reviewmicroservice3/ie/atu/reviewmicroservice3/ReviewMicroService3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.reviewmicroservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMicroService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewMicroService3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/reviewmicroservice3/ReviewMicroService3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.reviewmicroservice3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewMicroService3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
